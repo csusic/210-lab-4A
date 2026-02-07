d9e7b84d77e57b5e394a96d1dac23acef78b75ce
Christine Susic <csusic022@insite.4cd.edu>
2026-02-06 18:20:14 -0800

    Commit

--- main.cpp
+++ main.cpp
@@ -13,28 +13,26 @@ struct Color {
     int blue;
 };
 
+const int PREC = 2; //precision 
 const int MIN = 25, MAX = 50; //range of 25-50
 
 int main() {
+    cout << fixed << setprecision(PREC);
+    //random number
+    int n; 
     srand(time(0));
+    n = rand() % (MAX - MIN + 1) + MIN;
+    
     vector<Color> colorList;
     
     Color c = {45, 23, 78};
     cout << c.red << "\n";
     cout << c.green << "\n";
     cout << c.blue << "\n";
+    cout << n;
     
-    //Color c;
-    //c.red = (9);
-    //cout << c.red; 
-    
-    //fot (int i : v) 
-        //cout << i << " ";
-    
-    /*for (int i = 0; i < 5; i++)
-        myVec.push_back(i);
-    for (int val : myVec) cout << val << " ";
-    cout << endl << endl;*/
+    //for (int i = 0; i < n; i++)
+        //c.red.push_back(n);
     
     return 0;
 }
