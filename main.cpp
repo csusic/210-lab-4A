71f0537065fef2cef38dabb95e9ca2b4e04b9e74
Christine Susic <csusic022@insite.4cd.edu>
2026-02-06 19:19:35 -0800

    Milestone 3

--- main.cpp
+++ main.cpp
@@ -15,18 +15,20 @@ struct Color {
 
 //set column widths
 const int W1 = 10;
-//range of 25-50
+//range of 25-50 for random numbers
 const int MIN = 25, MAX = 50; 
 
 int main() {
     int colorNum = 0; //counter
-    //random number
-    int n; 
+    //random numbers
+    int n, n1, n2, n3; 
     srand(time(0));
     n = rand() % (MAX - MIN + 1) + MIN;
     
     vector<Color> colorList;
+    vector<int> myVec;
     
+    //output
     cout << left;
     cout << setw(W1) << left << "Color # ";
     cout << setw(W1) << "R value ";
@@ -36,26 +38,24 @@ int main() {
     cout << setw(W1) << "-------";
     cout << setw(W1) << "-------";
     cout << setw(W1) << "-------" << endl;
+    
+    /*for (int i = 0; i < 5; i++)
+        myVec.push_back(i);
+    for (int val : myVec) cout << val << " ";*/
          
     for (int i = 0; i < n; i++) {
         colorNum++; //counter
-        Color t = {n, n, n}; //temporary struct
+        //random numbers for rgb
+        n1 = rand() % (MAX - MIN + 1) + MIN;
+        n2 = rand() % (MAX - MIN + 1) + MIN;
+        n3 = rand() % (MAX - MIN + 1) + MIN;
+        Color t = {n1, n2, n3}; //temporary struct
         cout << setw(W1) << left <<  colorNum;
         cout << setw(W1) << t.red;
         cout << setw(W1) << t.green;
         cout << setw(W1) << t.blue;
-        cout << endl;
+        cout << endl; 
     }
-    
-    /*colorList.push_back(9);
-    //cout << 
-    
-    for (int i : colorList)
-        cout << i << " ";*/
-    
-    //for (int i = 0; i < n; i++)
-        //Color t = {n, n, n};
-        //colorList.push_back(n);
     
     return 0;
 }
